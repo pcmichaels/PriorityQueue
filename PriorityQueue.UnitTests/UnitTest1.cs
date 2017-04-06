0676d05a13876a7066f272d7809007a78cb5130b
pcmichaels <pcmichaels@live.com>
2017-04-06 07:44:08 +0100

    Create a simple test and add function

--- PriorityQueue.UnitTests/UnitTest1.cs
+++ PriorityQueue.UnitTests/UnitTest1.cs
@@ -8,9 +8,17 @@ namespace PriorityQueue.UnitTests
     public class BasicTestQueue
     {
         [Test]
-        public void QueueAdd
+        public void QueueAdd()
         {
+            // Arrange
+            PQueue.PriorityQueue<string> queue = new PQueue.PriorityQueue<string>();
 
+            // Act
+            queue.Add("test");
+            queue.Add("test2");
+
+            // Assert
+            Assert.AreEqual(2, queue.Count());
         }
     }
 }
---
+++ PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PQueue
+{
+    public class PriorityQueue<T>
+    {
+        public void Add(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Count()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
