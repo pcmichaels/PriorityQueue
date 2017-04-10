c20286e2995c9e20ca177de1aa706e88b382d61e
pcmichaels <pcmichaels@live.com>
2017-04-10 17:38:09 +0100

    All tests now pass, which means there's something wrong with them

--- PriorityQueue/PriorityQueue.cs
+++ PriorityQueue/PriorityQueue.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PQueue
 {
@@ -37,7 +34,7 @@ namespace PQueue
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            return _list.FirstOrDefault();
         }
     }
 }
